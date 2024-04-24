bcd9227cab4378ca47b5d5eea471822364674178
WiltonLio <wilton.lio@gmail.com>
2024-04-24 15:28:41 -0300

    Criando tests

---
+++ tests/Cepedi.Serasa.Pagamento.Api.Tests/CredorControllerTests.cs
@@ -0,0 +1,32 @@
+using Castle.Core.Logging;
+using Cepedi.Serasa.Pagamento.Api.Controllers;
+using Cepedi.Serasa.Pagamento.Compartilhado.Requests;
+using Cepedi.Serasa.Pagamento.Compartilhado.Responses;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using OperationResult;
+
+namespace Cepedi.Serasa.Pagamento.Api.Tests;
+
+public class CredorControllerTests
+{
+    private readonly IMediator _mediator = Substitute.For<IMediator>();
+    private readonly ILogger<CredorController> _logger = Substitute.For<ILogger<CredorController>>();
+    private readonly CredorController _sut;
+
+    public CredorControllerTests(){
+        _sut = new CredorController(_logger, _mediator);
+    }
+
+    [Fact]
+    public async Task CriarCredor_DeveEnviarRequest_Para_Mediator()
+    {
+        var request = new CriarCredorRequest { Nome = "Wilton"};
+        _mediator.Send(request).ReturnsForAnyArgs(Result.Success(new CriarCredorResponse(1, "")));
+
+        await _sut.CriarCredorAsync(request);
+
+        await _mediator.ReceivedWithAnyArgs().Send(request);
+    }
+}
---
+++ tests/Cepedi.Serasa.Pagamento.Dominio.Tests/Handler/AtualizarCredorRequestHandler.cs
@@ -0,0 +1,35 @@
+using Cepedi.Serasa.Pagamento.Compartilhado.Requests;
+using Cepedi.Serasa.Pagamento.Compartilhado.Responses;
+using Cepedi.Serasa.Pagamento.Dominio.Entidades;
+using Cepedi.Serasa.Pagamento.Dominio.Handlers;
+using Cepedi.Serasa.Pagamento.Dominio.Repositorio;
+using FluentAssertions;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NSubstitute;
+using OperationResult;
+
+namespace Cepedi.Serasa.Pagamento.Dominio.Tests;
+
+public class CriarCredorRequestHandlerTests
+{
+    private readonly ICredorRepository _CredorRepository = Substitute.For<ICredorRepository>();
+    private readonly ILogger<CriarCredorRequestHandler> _logger = Substitute.For<ILogger<CriarCredorRequestHandler>>();
+    private readonly CriarCredorRequestHandler _sut;
+
+    public CriarCredorRequestHandlerTests()
+    {
+        _sut = new CriarCredorRequestHandler(_logger, _CredorRepository);
+    }
+
+    [Fact]
+    public async Task CriarCredorAsync_QuandoCriar_DeveRetornarSucesso()
+    {
+        var Credor = new CriarCredorRequest { Nome = "Wilton" };
+        _CredorRepository.CriarCredorAsync(It.IsAny<CredorEntity>()).ReturnsForAnyArgs(new CredorEntity());
+        var result = await _sut.Handle(Credor, CancellationToken.None);
+
+        result.Should().BeOfType<Result<CriarCredorResponse>>().Which.Value.Nome.Should().Be(Credor.Nome);
+        result.Should().BeOfType<Result<CriarCredorResponse>>().Which.Value.Nome.Should().NotBeEmpty();
+    }
+}
