fe8bbf179d1366997c896c46ccfc073f73d83efb
WelvisSS <welvisconta@gmail.com>
2024-04-25 15:48:06 -0300

    Refactor code in AtualizarDividaRequest.cs, CriarDividaResponse.cs, AtualizarDividaRequestHandler.cs, CriarDividaRequestHandler.cs, and CriarPagamentoRequestHandler.cs

--- src/Cepedi.Serasa.Pagamento.Compartilhado/Responses/CriarDividaResponse.cs
+++ src/Cepedi.Serasa.Pagamento.Compartilhado/Responses/CriarDividaResponse.cs
@@ -1,9 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Cepedi.Serasa.Pagamento.Compartilhado.Responses;
+﻿namespace Cepedi.Serasa.Pagamento.Compartilhado.Responses;
 
 public record CriarDividaResponse(int IdDivida, double Valor, DateTime DataDeVencimento);
--- src/Cepedi.Serasa.Pagamento.Dominio/Handlers/CriarDividaRequestHandler.cs
+++ src/Cepedi.Serasa.Pagamento.Dominio/Handlers/CriarDividaRequestHandler.cs
@@ -11,20 +11,25 @@ namespace Cepedi.Serasa.Pagamento.Dominio;
 public class CriarDividaRequestHandler
     : IRequestHandler<CriarDividaRequest, Result<CriarDividaResponse>>
 {
-    private readonly ILogger _logger;
+    private readonly ILogger<CriarDividaRequestHandler> _logger;
     private readonly IDividaRepository _dividaRepository;
+
+    private readonly IPessoaRepository _pessoaRepository;
 
     private readonly ICredorRepository _credorRepository;
 
-    public CriarDividaRequestHandler(ILogger logger, IDividaRepository dividaRepository, ICredorRepository credorRepository){
+    public CriarDividaRequestHandler(ILogger<CriarDividaRequestHandler> logger, IDividaRepository dividaRepository, ICredorRepository credorRepository, IPessoaRepository pessoaRepository)
+    {
         _logger = logger;
         _dividaRepository = dividaRepository;
         _credorRepository = credorRepository;
+        _pessoaRepository = pessoaRepository;
     }
     public async Task<Result<CriarDividaResponse>> Handle(CriarDividaRequest request, CancellationToken cancellationToken)
     {
         var credorEntity = await _credorRepository.ObterCredorAsync(request.IdCredor);
-        var pessoaEntity = await _dividaRepository.ObterPessoaAsync(request.IdPessoa);
+        var pessoaEntity = await _pessoaRepository.ObterPessoaAsync(request.IdPessoa);
+
         var divida = new DividaEntity()
         {
             IdCredor = request.IdCredor,
