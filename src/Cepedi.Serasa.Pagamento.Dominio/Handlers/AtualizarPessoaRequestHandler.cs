5a7d07c28bdd1c70c3300e464f45e7f795c19cf3
WelvisSS <welvisconta@gmail.com>
2024-05-21 12:03:19 -0300

    add save cache

--- src/Cepedi.Serasa.Pagamento.Dominio/Handlers/AtualizarPessoaRequestHandler.cs
+++ src/Cepedi.Serasa.Pagamento.Dominio/Handlers/AtualizarPessoaRequestHandler.cs
@@ -1,7 +1,9 @@
 ﻿using Cepedi.Serasa.Pagamento.Compartilhado.Requests;
 using Cepedi.Serasa.Pagamento.Compartilhado.Responses;
+using Cepedi.Serasa.Pagamento.Dominio.Entidades;
 using Cepedi.Serasa.Pagamento.Dominio.Repositorio;
 using Cepedi.Serasa.Pagamento.Dominio.Repositorio.Queries;
+using Cepedi.Serasa.Pagamento.Dominio.Servicos;
 using MediatR;
 using Microsoft.Extensions.Logging;
 using OperationResult;
@@ -16,11 +18,14 @@ public class AtualizarPessoaRequestHandler : IRequestHandler<AtualizarPessoaRequ
     private readonly IPessoaRepository _pessoaRepository;
 
     private readonly ILogger<AtualizarPessoaRequestHandler> _logger;
-    public AtualizarPessoaRequestHandler(IPessoaRepository pessoaRepository, IPessoaQueryRepository pessoaQueryRepository, IUnitOfWork unitOfWork, ILogger<AtualizarPessoaRequestHandler> logger)
+
+    private readonly ICache<PessoaEntity> _cache;
+    public AtualizarPessoaRequestHandler(IPessoaRepository pessoaRepository, IPessoaQueryRepository pessoaQueryRepository, ICache<PessoaEntity> cache, IUnitOfWork unitOfWork, ILogger<AtualizarPessoaRequestHandler> logger)
     {
         _pessoaQueryRepository = pessoaQueryRepository;
         _pessoaRepository = pessoaRepository;
         _unitOfWork = unitOfWork;
+        _cache = cache;
         _logger = logger;
     }
     public async Task<Result<AtualizarPessoaResponse>> Handle(AtualizarPessoaRequest request, CancellationToken cancellationToken)
@@ -38,7 +43,11 @@ public class AtualizarPessoaRequestHandler : IRequestHandler<AtualizarPessoaRequ
 
         await _pessoaQueryRepository.AtualizarPessoaDapperAsync(pessoaEntity);
 
+        await _cache.SalvarAsync($"Pessoa:{pessoaEntity.Id}", pessoaEntity, 1800);
+
         await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+
 
         return Result.Success(new AtualizarPessoaResponse(pessoaEntity.Nome));
 
