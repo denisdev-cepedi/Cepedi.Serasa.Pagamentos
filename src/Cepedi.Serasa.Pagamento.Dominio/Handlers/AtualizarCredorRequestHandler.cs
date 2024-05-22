e985caec019f2b7d3b205c55b3a763aba2d14744
EduardoMatheus96 <eduardomatheusuesc@gmail.com>
2024-05-22 07:26:55 -0300

    feat/Implement UnitOFWork to Credor

--- src/Cepedi.Serasa.Pagamento.Dominio/Handlers/AtualizarCredorRequestHandler.cs
+++ src/Cepedi.Serasa.Pagamento.Dominio/Handlers/AtualizarCredorRequestHandler.cs
@@ -9,13 +9,15 @@ namespace Cepedi.Serasa.Pagamento.Dominio.Handlers;
 public class AtualizarCredorRequestHandler :
     IRequestHandler<AtualizarCredorRequest, Result<AtualizarCredorResponse>>
 {
+    private readonly IUnitOfWork _unitOfWork;
     private readonly ICredorRepository _CredorRepository;
     private readonly ILogger<AtualizarCredorRequestHandler> _logger;
 
-    public AtualizarCredorRequestHandler(ICredorRepository CredorRepository, ILogger<AtualizarCredorRequestHandler> logger)
+    public AtualizarCredorRequestHandler(ICredorRepository CredorRepository, ILogger<AtualizarCredorRequestHandler> logger, IUnitOfWork unitOfWork)
     {
         _CredorRepository = CredorRepository;
         _logger = logger;
+        _unitOfWork = unitOfWork;
     }
     //modificar
     public async Task<Result<AtualizarCredorResponse>> Handle(AtualizarCredorRequest request, CancellationToken cancellationToken)
@@ -32,6 +34,8 @@ public class AtualizarCredorRequestHandler :
         CredorEntity.Atualizar(request.Nome);
 
         await _CredorRepository.AtualizarCredorAsync(CredorEntity);
+
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
 
         return Result.Success(new AtualizarCredorResponse(CredorEntity.Nome));
     }
